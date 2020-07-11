047813e35aaecf88fcd0301e50e9d73d544404cc
Dileepa Samarawickrama <dansamarawicky@outlook.com>
2020-07-11 18:04:50 +0530

    Updates

---
+++ precache-manifest.17765cba2c215838f88d2d0ec45b5eaa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d03c50e79722ffa76d13f4ee8face616",
+    "url": "/react-todo-tasks-app/index.html"
+  },
+  {
+    "revision": "b34371fe5b8a870e998e",
+    "url": "/react-todo-tasks-app/static/css/main.83eadf53.chunk.css"
+  },
+  {
+    "revision": "f005f6725549e77f63a8",
+    "url": "/react-todo-tasks-app/static/js/2.d2673a93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-tasks-app/static/js/2.d2673a93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b34371fe5b8a870e998e",
+    "url": "/react-todo-tasks-app/static/js/main.364bb213.chunk.js"
+  },
+  {
+    "revision": "7b7d787588aec9b5f86b",
+    "url": "/react-todo-tasks-app/static/js/runtime-main.05ec3bb6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-todo-tasks-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
